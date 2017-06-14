116c019a8af5d07f2380f1f8e98ff99415bfe7aa
Artur Pinto <arturpsantost@gmail.com>
2017-06-14 20:32:54 -0300

    uma coisa

--- src/main/java/trab/main.java
+++ src/main/java/trab/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Tes!");
+		System.out.println("Uma coisa diferente");
 		// TODO Auto-generated method stub
 
 	}
