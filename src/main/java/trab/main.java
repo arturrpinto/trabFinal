74d41192d263276d1255fb020eea2ad61019118e
Artur Pinto <arturpsantost@gmail.com>
2017-06-14 21:16:00 -0300

    ok

--- src/main/java/trab/main.java
+++ src/main/java/trab/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Trabalho Roger")
+		System.out.println("Trabalho Roger"
 		// TODO Auto-generated method stub
 
 	}
